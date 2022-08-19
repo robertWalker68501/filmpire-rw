877d9bb9b37bbf6f43725a24b16e466a285a4116
Robert Walker <robertWalker6580@gmail.com>
2022-08-18 18:25:28 -0700

    Completed the home page layout

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,7 +1,43 @@
+import { useState, useEffect } from 'react';
+import {
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Typography,
+} from '@mui/material';
+import { useSelector } from 'react-redux';
+import { MovieList } from '../index';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
+
 const Movies = () => {
+  const { data, error, isFetching } = useGetMoviesQuery();
+
+  if (isFetching) {
+    return (
+      <Box display='flex' justifyContent='center'>
+        <CircularProgress size='4rem' />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display='flex' alignItems='center' mt='20px'>
+        <Typography variant='h4'>
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occurred.';
+
   return (
     <div>
-      <h1>Movies</h1>
+      <MovieList movies={data} />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import store from './app/store';
 
 const theme = createTheme({});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    {/*<React.StrictMode>*/}
     <ThemeProvider theme={theme}>
       <Router>
         <App />
       </Router>
     </ThemeProvider>
-  </React.StrictMode>
+    {/*</React.StrictMode>*/}
+  </Provider>
 );
